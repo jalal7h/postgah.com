feeb49c27747ee10d69a9b4ac0fc03316eda4954
Jalal Hoseini <jalal7h@gmail.com>
2016-09-02 21:54:49 +0430

    fix cat image display

--- components/pgCat/pgCat_image.php
+++ components/pgCat/pgCat_image.php
@@ -4,6 +4,8 @@ function pgCat_image( $rw, $size=null ){
 	
 	// $pgCat_image = 'data/cat_memo/'.rand(101,116).'.png';
 
+	$pgCat_image = $rw['logo'];
+
 	if( $size ){
 		$pgCat_image = _URL."/resize/$size/$pgCat_image";
 	}
