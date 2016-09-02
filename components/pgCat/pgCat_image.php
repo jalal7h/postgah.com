20db5187a48fca3f64546af3c949918062987353
Jalal Hoseini <jalal7h@gmail.com>
2016-09-02 21:37:17 +0430

    remove cat memo image

--- components/pgCat/pgCat_image.php
+++ components/pgCat/pgCat_image.php
@@ -2,7 +2,7 @@
 
 function pgCat_image( $rw, $size=null ){
 	
-	$pgCat_image = 'data/cat_memo/'.rand(101,116).'.png';
+	// $pgCat_image = 'data/cat_memo/'.rand(101,116).'.png';
 
 	if( $size ){
 		$pgCat_image = _URL."/resize/$size/$pgCat_image";
