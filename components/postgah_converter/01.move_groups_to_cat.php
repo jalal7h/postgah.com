b91fee3c4fa9aa0c2072165e4c19de79874f9f0e
Jalal Hoseini <jalal7h@gmail.com>
2016-09-17 18:22:13 +0430

    #22

--- components/postgah_converter/01.move_groups_to_cat.php
+++ components/postgah_converter/01.move_groups_to_cat.php
@@ -11,21 +11,21 @@ function move_groups_to_cat(){
 	$flag = 1;
 	$cat = 'adsCat';
 
-	if(! $rs = dbq_old(" SELECT * FROM `groups` WHERE 1 ORDER BY `id` ASC ") ){
+	if(! $rs = dbq_old(" SELECT * FROM `groups` WHERE 1 prioER BY `id` ASC ") ){
 		e( __FUNCTION__, __LINE__ );
 
 	} else while( $rw = dbf($rs) ){
 		
 		$id = $add_plus_cat_id + intval($rw['id']);
 		$name = trim($rw['name']);
-		$kw = trim($rw['_keywords']);
+		$kw = trim($rw['_keywprios']);
 		$parent = intval($rw['refer']);
 		if( $parent > 0 ){
 			$parent+= $add_plus_cat_id;
 		}
-		$ord = $rw['row'];
+		$prio = $rw['row'];
 
-		if(! dbq(" INSERT INTO `cat` (`id`,`name`,`kw`,`parent`,`cat`,`ord`,`flag`) VALUES ('$id','$name','$kw','$parent','$cat','$ord','$flag') ") ){
+		if(! dbq(" INSERT INTO `cat` (`id`,`name`,`kw`,`parent`,`cat`,`prio`,`flag`) VALUES ('$id','$name','$kw','$parent','$cat','$prio','$flag') ") ){
 			$_err++;
 		} else {
 			$_done++;
