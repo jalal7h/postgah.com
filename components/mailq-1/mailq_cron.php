43ce9c7ccd5cb57710a943c4224d53f7a1c090d7
Jalal Hoseini <jalal7h@gmail.com>
2016-09-18 19:16:47 +0430

    fix n6

--- components/mailq-1/mailq_cron.php
+++ components/mailq-1/mailq_cron.php
@@ -1,16 +1,33 @@
 <?
+
+# jalal7h@gmail.com
+# 2016/09/18
+# 1.1
+
 $GLOBALS['do_action'][] = 'mailq_cron';
 
 function mailq_cron(){
 	
-	$count = 1;
+	$limit = 1;
 	
-	if(!$rs = dbq(" SELECT * FROM `mailq` WHERE 1 ORDER BY `id` ASC LIMIT $count ")){
-		e("error on mailq_cron - ".__LINE__);
+	if(! $rs = dbq(" SELECT * FROM `mailq` WHERE 1 ORDER BY `id` ASC LIMIT $limit ") ){
+		dg();
 	
-	} else while($rw = dbf($rs)){
+	} else if(! dbn($rs) ){
+		echo "no record to send";
+
+	} else while( $rw = dbf($rs) ){
+		
 		xmail( $rw['to'] , $rw['subject'] , $rw['text'] , $rw['mail_from'] , ($rw['html']=='1'?true:false) , $cron=true );
-		dbq(" DELETE FROM `mailq` WHERE `id`='".$rw['id']."' LIMIT 1 ");
+
+		if(! dbq(" DELETE FROM `mailq` WHERE `id`='".$rw['id']."' LIMIT 1 ") ){
+			dg();
+		}
+
 	}
 }
+
+
+
+
 
