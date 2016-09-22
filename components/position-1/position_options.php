9fe50d21c3ae54cd98c651361aaed7cdb7302e37
Jalal Hoseini <jalal7h@gmail.com>
2016-09-22 22:03:58 +0330

    fix n1

--- components/position-1/position_options.php
+++ components/position-1/position_options.php
@@ -1,17 +1,23 @@
 <?
 
 function position_options( $type , $array=true ){
-	if(!$rs01 = dbq(" SELECT * FROM `position` WHERE `type`='$type' ORDER BY `name` ")){
+	
+	if(! $rs01 = dbq(" SELECT * FROM `position` WHERE `type`='$type' ORDER BY `name` ") ){
 		echo "err - ".__LINE__;
-	} else while($rw01 = dbf($rs01)){
-		if($array){
+	
+	} else while( $rw01 = dbf($rs01) ){
+		
+		if( $array ){
 			$c[ $rw01['id'] ] = $rw01['name'];
+		
 		} else {
 			$c.= "<option value='".$rw01['id']."' >".$rw01['name']."</option>";
 		}
+	
 	}
 
 	return $c;
+	
 }
 
 
