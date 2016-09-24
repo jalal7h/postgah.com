a21d1f4ee077256b9049b1dcfd482e080283f78f
Jalal Hoseini <jalal7h@gmail.com>
2016-09-24 21:25:51 +0330

    fix n1

--- components/layout-1/layout_page_link.php
+++ components/layout-1/layout_page_link.php
@@ -1,5 +1,8 @@
 <?php
 
+# jalal7h@gmail.com
+# 2016/09/24
+# 1.1
 
 function layout_page_link( $rw_page ){
 
@@ -7,9 +10,12 @@ function layout_page_link( $rw_page ){
 		$rw_page = table( 'page', $rw_page );
 	}
 
-	$link = _URL.'/page-'.$rw_page['id'].'.html';
+	if( $rw_page['id'] == 1 ){
+		return _URL;
 
-	return $link ;
+	} else {
+		return _URL.'/page-'.$rw_page['id'].'.html';
+	}
 
 }
 
