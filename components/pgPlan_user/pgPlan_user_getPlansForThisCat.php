1c4dce8ab144d4f950e8f9658a665d3cd394e640
Jalal Hoseini <jalal7h@gmail.com>
2016-11-07 01:03:34 +0330

    fix n7

--- components/pgPlan_user/pgPlan_user_getPlansForThisCat.php
+++ components/pgPlan_user/pgPlan_user_getPlansForThisCat.php
@@ -8,7 +8,7 @@ function pgPlan_user_getPlansForThisCat( $ignore_plan_id_list=null ){
 		$ignore_plan_id_query = " AND `id` NOT IN (".implode( ",", $ignore_plan_id_list ).") ";
 	}
 
-	echo "<div class=\"ppugpftc\">\n";
+	// echo "<div class=\"ppugpftc\">\n";
 
 	#
 	# vars
@@ -48,44 +48,52 @@ function pgPlan_user_getPlansForThisCat( $ignore_plan_id_list=null ){
 		// .pgiul_sb_".$item_id." { display: none !important; }
 		// </style>";
 
-	} else while( $rw = dbf($rs) ){
+	} else {
+		
+		echo "<div class=\"ppugpftc\">\n";
+		
+		while( $rw = dbf($rs) ){
 
-		if(! $rs0 = dbq(" SELECT * FROM `plan_duration` WHERE `plan_id`='".$rw['id']."' ORDER BY `name` ASC ") ){
-			e(__FUNCTION__,__LINE__);
+			if(! $rs0 = dbq(" SELECT * FROM `plan_duration` WHERE `plan_id`='".$rw['id']."' ORDER BY `name` ASC ") ){
+				e(__FUNCTION__,__LINE__);
 
-		} else if(! dbn($rs0) ){
-			;//
+			} else if(! dbn($rs0) ){
+				;//
 
-		} else {
-
-			echo "<span class='this_plan'>";
-
-			#
-			# head
-			if(! $rw['cat_id'] ){
-				$plan_head_cat = 'همه دسته بندی ها';
 			} else {
-				$plan_head_cat = cat_translate( $rw['cat_id'] );	
+
+				echo "<span class='this_plan'>";
+
+				#
+				# head
+				if(! $rw['cat_id'] ){
+					$plan_head_cat = 'همه دسته بندی ها';
+				} else {
+					$plan_head_cat = cat_translate( $rw['cat_id'] );	
+				}
+				if(! $rw['position_id'] ){
+					$plan_head_pos = '';
+				} else {
+					$plan_head_pos = "در ".position_translate( $rw['position_id'] );
+				}
+
+				echo "<div class='head'>پلان های $plan_head_cat $plan_head_pos</div>";
+
+				while( $rw0 = dbf($rs0) ){
+					// '.($rw0['id']==$item_planDurationId?'checked':'').'
+					echo '<label><input type="radio" name="plan_duration_id" value="'.$rw0['id'].'" /> '.$rw['name_on_form'].' ( '.$rw0['name'].' / '.number_format($rw0['cost']).' '.setting('money_unit').' ) '.( $rw['sample_page'] ? '<a class="sample_page" href="'.$rw['sample_page'].'" target="_blank" >'.lmtc('plan:sample_page').'</a>' : '').'</label>';
+				}
+
+				echo "</span>";
+
 			}
-			if(! $rw['position_id'] ){
-				$plan_head_pos = '';
-			} else {
-				$plan_head_pos = "در ".position_translate( $rw['position_id'] );
-			}
-
-			echo "<div class='head'>پلان های $plan_head_cat $plan_head_pos</div>";
-
-			while( $rw0 = dbf($rs0) ){
-				// '.($rw0['id']==$item_planDurationId?'checked':'').'
-				echo '<label><input type="radio" name="plan_duration_id" value="'.$rw0['id'].'" /> '.$rw['name_on_form'].' ( '.$rw0['name'].' / '.number_format($rw0['cost']).' '.setting('money_unit').' ) '.( $rw['sample_page'] ? '<a class="sample_page" href="'.$rw['sample_page'].'" target="_blank" >'.lmtc('plan:sample_page').'</a>' : '').'</label>';
-			}
-
-			echo "</span>";
-
 		}
+
+		echo "</div>\n";
+		
 	}
 
-	echo "</div>\n";
+	// echo "</div>\n";
 
 }
 
