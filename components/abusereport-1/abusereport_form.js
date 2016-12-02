79087f322c152d3d3b617f5f2efa19cdedf61b84
Jalal Hoseini <jalal7h@gmail.com>
2016-12-03 00:08:25 +0330

    fix n1

--- components/abusereport-1/abusereport_form.js
+++ components/abusereport-1/abusereport_form.js
@@ -4,11 +4,11 @@ jQuery(document).ready(function($) {
 	$("body").delegate( ".abusereport_form", "submit", function( e ) {
 		
 		if( $(this).find('textarea').val() == '' ){
-			$(this).find('textarea').css({'border':'1px solid orangered'});
+			$(this).find('textarea').css({'box-shadow':'rgba(255, 69, 0, 0.42) 0px 0px 90px inset'});
 			return false;
 		
 		} else {
-			$(this).find('textarea').css({'border':''});
+			$(this).find('textarea').css({'box-shadow':''});
 		}
 
 		if( $(this).find('select').val() == '' ){
