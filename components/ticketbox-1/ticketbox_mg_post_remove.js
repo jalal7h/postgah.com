dcb602202b43f1831a6ba52801fd3f0dc2cad899
Jalal Hoseini <jalal7h@gmail.com>
2016-12-19 12:16:21 +0330

    add confirm on remove for ticketbox post in admin

--- components/ticketbox-1/ticketbox_mg_post_remove.js
+++ components/ticketbox-1/ticketbox_mg_post_remove.js
@@ -5,43 +5,45 @@ jQuery(document).ready(function($) {
 
 	// remove button
 	$('body').delegate('.ticketbox_view .post .remove', 'click', function() {
-	cl('dslkj');	
-		
+
 		rm = $(this);
 		ps = rm.parent();
-		post_id = ps.attr('post_id');
 
-		
-		if( ticketbox_mg_post_remove_instantly_hide == 1 ){
-			ps.animate({'height':'0','margin':'-10', 'opacity':'0.0'}, 100, function(){
-				ps.remove();
-			});
-		
-		} else {
-			rm.css({'opacity':'0'});
-			ps.css({'opacity':'0.8'});
-		}
+		if( confirm( rm.attr('text_remove') ) ){
 
-
-		$.ajax({
-			url: "./?do_action=ticketbox_mg_post_remove&post_id="+post_id
-
-		}).done(function( html ) {
+			post_id = ps.attr('post_id');
 			
-			if( html == 'OK' ){
-				ps.animate({'height':'0','margin':'-10', 'opacity':'0.0'}, 200, function(){
+			if( ticketbox_mg_post_remove_instantly_hide == 1 ){
+				ps.animate({'height':'0','margin':'-10', 'opacity':'0.0'}, 100, function(){
 					ps.remove();
 				});
 			
-			} else if( html == 'NULL' ){
-				location.href = './?page=admin&cp=ticketbox_mg';
-
 			} else {
-				rm.css({'opacity':'1'});
-				// ps.css({'opacity':'1'});
+				rm.css({'opacity':'0'});
+				ps.css({'opacity':'0.8'});
 			}
 
-		});
+
+			$.ajax({
+				url: "./?do_action=ticketbox_mg_post_remove&post_id="+post_id
+
+			}).done(function( html ) {
+				
+				if( html == 'OK' ){
+					ps.animate({'height':'0','margin':'-10', 'opacity':'0.0'}, 200, function(){
+						ps.remove();
+					});
+				
+				} else if( html == 'NULL' ){
+					location.href = './?page=admin&cp=ticketbox_mg';
+
+				} else {
+					rm.css({'opacity':'1'});
+				}
+
+			});
+
+		}
 
 	});
 
--- components/ticketbox-1/ticketbox_view_post.php
+++ components/ticketbox-1/ticketbox_view_post.php
@@ -5,7 +5,7 @@ function ticketbox_view_post( $rw ){
 	$c.= "
 	<div class=\"post\" post_id=\"".$rw['id']."\" >
 		
-		".( is_admin() ? "<div class=\"remove\"></div>" : '' )."
+		".( is_admin() ? "<div class=\"remove\" text_remove=\"".__('آیا مایل به حذف هستید؟')."\" ></div>" : '' )."
 		
 		<div class=\"info ".( is_adminUser($rw['user_id']) ? 'admin' : 'user' )."\">
 			".( is_component('useravatar') ? useravatar( $rw['user_id'] ) : '' )."
