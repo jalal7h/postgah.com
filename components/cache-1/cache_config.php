7f73cd66a0b0f7d0f6e55207d3dcbb3232430796
Jalal Hoseini <jalal7h@gmail.com>
2016-12-21 15:57:08 +0330

    fix cache config.

--- components/cache-1/cache_config.php
+++ components/cache-1/cache_config.php
@@ -1,12 +1,14 @@
 <?
 
 # jalal7h@gmail.com
-# 2016/08/31
-# 1.1
+# 2016/12/21
+# 1.2
 
 # -spi-
 
-define( '_cache_flag', false );
+if(! defined('_cache_flag') ){
+	define( '_cache_flag', false );
+}
 define( '_cache_dir', 'data/cache' );
 define( '_cache_debug', false );
 
