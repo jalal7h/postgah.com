009d580c7978aa87009a454e370094e6c15ea889
Jalal Hoseini <jalal7h@gmail.com>
2016-12-26 16:18:50 +0330

    fix n2

--- components/main-3/redirect_to_non_www.php
+++ components/main-3/redirect_to_non_www.php
@@ -4,11 +4,16 @@
 # 2016/12/26
 # 1.0
 
-if( strstr( _FULL_URL , '://www.' ) ){
-	$new_FULL_URL = str_replace( '://www.' , '://' , _FULL_URL );
+if( strstr( _URL , '://www.' ) ){
+
+	$new_FULL_URL = str_replace( '://www.' , '://' , _URL );
+	$new_FULL_URL.= urldecode($_SERVER['REQUEST_URI']);
+
 	header( "HTTP/1.1 301 Moved Permanently" ); 
 	header( "Location: ".$new_FULL_URL ); 
+	
 	die();
+	
 }
 
 
