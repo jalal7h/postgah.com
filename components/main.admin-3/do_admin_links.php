63bd6f39e012beefef4407cb78bf03b1f57d54d2
Jalal Hoseini <jalal7h@gmail.com>
2017-01-02 00:05:37 +0330

    fix admin logout

--- components/main.admin-3/do_admin_links.php
+++ components/main.admin-3/do_admin_links.php
@@ -8,7 +8,7 @@ function do_admin_links(){
 	?>
 	<div class="links">
 		<a href="#" id="admin_changepassword"><?=__('پروفایل مدیریت')?></a>
-		<a href="<?=_URL?>/logout"><?=__('خروج')?></a>
+		<a href="<?=_URL?>/admin/logout"><?=__('خروج')?></a>
 	</div>
 	<?
 }
