0346d5f6dff8fd559617b0ab3b8708cae1a0d9e6
Jalal Hoseini <jalal7h@gmail.com>
2017-01-04 13:18:54 +0330

    fix q filters

--- components/pgSearch/pgSearch_q.php
+++ components/pgSearch/pgSearch_q.php
@@ -1,8 +1,8 @@
 <?
 
 # jalal7h@gmail.com
-# 2016/01/02
-# 1.2
+# 2016/01/04
+# 1.3
 
 function pgSearch_q(){
 	
@@ -12,8 +12,8 @@ function pgSearch_q(){
 	} else if(! $q = rawurldecode($q) ){
 		e();
 
-	// } else if(! $q = mb_ereg_replace( '[^A-Za-z0-9آ-ی ]+', '', $q ) ){
-	//	e();
+	} else if(! $q = mb_ereg_replace( '[^A-Za-z0-9آ-ی ]+', '', $q ) ){
+		e();
 
 	} else if(! $q = trim($q) ){
 		e();
