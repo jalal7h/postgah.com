85cd9d14da4baffda0bfb06f1e92bf72ffb66763
Jalal Hoseini <jalal7h@gmail.com>
2017-01-06 09:23:07 +0330

    fix cache time on tags

---
+++ components/cache-1/cache_hit_timeout_to_second.php
@@ -0,0 +1,68 @@
+<?php
+
+# jalal7h@gmail.com
+# 2017/01/06
+# 1.0
+
+# 3600/3600second/10minute/6hour/2day/3week/3month/3season/1year/null/1hours-2hours
+
+function cache_hit_timeout_to_second( $timeout ){
+
+	if( strstr( $timeout , '-' ) ){
+		$timeout = explode( '-', $timeout );
+		$time_a = $timeout[0];
+		$time_a = cache_hit_timeout_to_second( $time_a );
+		$time_b = $timeout[1];
+		$time_b = cache_hit_timeout_to_second( $time_b );
+		return rand( $time_a , $time_b );
+	}
+	
+	$timeout = trim( $timeout );
+	$timeout_numb = mb_ereg_replace( '[^0-9]+', '', $timeout );
+	$timeout_text = mb_ereg_replace( '[^A-Za-z]+', '', $timeout );
+	$timeout_text = strtolower($timeout_text);
+
+	switch ( $timeout_text ) {
+		
+		case 'year':
+		case 'years':
+			$timeout_numb*= 31536000;
+			break;
+		
+		case 'month':
+		case 'months':
+			$timeout_numb*= 2592000;
+			break;
+
+		case 'week':
+		case 'weeks':
+			$timeout_numb*= 604800;
+			break;
+		
+		case 'day':
+		case 'days':
+			$timeout_numb*= 86400;
+			break;
+
+		case 'hour':
+		case 'hours':
+			$timeout_numb*= 3600;
+			break;
+
+		case 'minute':
+		case 'minutes':
+			$timeout_numb*= 60;
+			break;
+		
+		case 'second':
+		default:
+			//
+			break;
+	}
+
+	return $timeout_numb;
+
+}
+
+
+
--- components/kword-1/seo_sitemap.php
+++ components/kword-1/seo_sitemap.php
@@ -1,15 +1,16 @@
 <?
 
 # jalal7h@gmail.com
-# 2017/01/02
-# 1.1
+# 2017/01/06
+# 1.2
 
 $GLOBALS['seo_sitemap']['tags'] = array( 
 	
-	"query"	=>	" SELECT * FROM `kword` WHERE 1 ORDER BY `id` ASC ",
-	"link"	=>	'kword_link($rw["kword"])',
-	"freq"	=>	"weekly",
-	"prio"	=>	"0.5",
+	"query"		 =>	" SELECT * FROM `kword` WHERE 1 ORDER BY `id` ASC ",
+	"link"		 =>	'kword_link($rw["kword"])',
+	"freq"		 =>	"weekly",
+	"prio"		 =>	"0.5",
+	"cache_time" =>	"7days-8days",
 
 );
 	
