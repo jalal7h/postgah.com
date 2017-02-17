b245d12c4b9162ee11b9d6885f9b0c037569aa51
Jalal Hoseini <jalal7h@gmail.com>
2017-02-18 00:36:18 +0330

    .

--- templates/Default/template/header.template.php
+++ templates/Default/template/header.template.php
@@ -4,14 +4,17 @@
 	<div class="middle">
 		
 		<a alt="{main_title}" title="{main_title}" class="logo-container" href="{_URL}"><img alt="{main_title}" title="{main_title}" src="{_URL}/{site_logo}"/></a>
-		
+
 		{pgSearch_form}
 
-		<a href="{_URL}/<?=Slug::get('page',14)?>/items/new" class="new_ads">ارسال آگهی رایگان</a>
-		<?if( user_logged() ):?>
-			<a href="{_URL}/<?=Slug::get('page',14)?>" class="login_or_register">محیط کاربری</a>
-		<?else:?>
+		<?if(! user_logged() ):?>
 			<a href="{_URL}/<?=Slug::get('page',60)?>" class="login_or_register">ورود / عضویت</a>
+		<?elseif(! is_userPanel() ):?>
+			<a href="{_URL}/<?=Slug::get('page',14)?>" class="login_or_register">محیط کاربری</a>
+		<?endif?>
+
+		<?if(! strstr($_SERVER['REQUEST_URI'], '/userpanel/items/new' ) ):?>
+			<a href="{_URL}/<?=Slug::get('page',14)?>/items/new" class="new_ads">ارسال آگهی رایگان</a>
 		<?endif?>
 
 		{breadcrumb}
