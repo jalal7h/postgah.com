991eb2c702f5a4702ea073d8a1eaf5eee340cf50
Jalal Hoseini <jalal7h@gmail.com>
2017-04-02 01:27:01 +0430

    .

--- components/userverification-1/userverification_cell_send.php
+++ components/userverification-1/userverification_cell_send.php
@@ -1,8 +1,8 @@
 <?php
 
 # jalal7h@gmail.com
-# 2017/03/12
-# 1.0
+# 2017/04/01
+# 1.1
 
 add_action('userverification_cell_send');
 
@@ -18,7 +18,7 @@ function userverification_cell_send(){
 		dg( $key );
 		
 		if( que::pop('userverification_cell_keyGenerateOrFind_thisIsNew') == 1 ){
-			texty_sms( $cell, 'userverification', [ 'verification_code'=>$key ] );
+			texty_sms( $cell, 'userverification', [ 'verify_code'=>$key, 'tiny_title'=>setting('tiny_title') ] );
 		}
 		
 		echo userverification_cell_keyTimeRemined();
