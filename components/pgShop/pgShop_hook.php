e80ea63865c377aab5105744e8a17e4343275751
Jalal Hoseini <jalal7h@gmail.com>
2017-05-05 22:36:23 +0430

    .

--- components/pgShop/pgShop_hook.php
+++ components/pgShop/pgShop_hook.php
@@ -4,12 +4,13 @@
 # 2017/05/04
 # 1.0
 
-if( !$_REQUEST['page'] and $_SERVER['SERVER_NAME'] == 'localhost' ){
+if( defined('_SUB') and !$_REQUEST['page'] ){
 	
 	$_REQUEST['page'] = 171;
 	$_GET['page'] = 171;
 	
-	// define( '_SHOP_PATH', 'hpe.'._DOMAIN );
-	define( '_SHOP_PATH', 'hpe.postgah.com' );
+	define( '_SHOP_PATH', _DOMAIN );
 
 }
+
+
