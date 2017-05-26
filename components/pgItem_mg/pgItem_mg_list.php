b18226492a8d46236314d99d720a147c560d3de9
Jalal Hoseini <jalal7h@gmail.com>
2017-05-26 18:31:22 +0430

    .

--- components/pgItem_mg/pgItem_mg_list.php
+++ components/pgItem_mg/pgItem_mg_list.php
@@ -54,7 +54,7 @@ function pgItem_mg_list(){
 
 	#
 	# search columns // az in field ha tu table search mikone
-	$list['search'] = [ "id", "name", "text", "user(user_id)[username]", "user(user_id)[name]", "cat(cat_id)[name]", "position(position_id)[name]" ];
+	$list['search'] = [ "id", "name", "text", "user(user_id)[email]", "user(user_id)[name]", "cat(cat_id)[name]", "position(position_id)[name]" ];
 
 	#
 	# paging select
