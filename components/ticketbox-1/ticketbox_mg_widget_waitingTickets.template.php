b09daaa0f1337cbc586335bf55ff7be0130d21f0
Jalal Hoseini <jalal7h@gmail.com>
2017-06-08 02:00:13 +0430

    .

--- components/ticketbox-1/ticketbox_mg_widget_waitingTickets.template.php
+++ components/ticketbox-1/ticketbox_mg_widget_waitingTickets.template.php
@@ -1,4 +1,4 @@
 
-<div class="head"><lang>آخرین تیکت ها</lang></div>
+<div class="head"><lang>آخرین تیکت ها</lang> (<span class="fa-blink" style="color: #f44">live</span>)</div>
 <div class="list">...</div>
 
