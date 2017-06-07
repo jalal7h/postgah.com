69b089b6e544fed6771dacc9fb5139613eb4e464
Jalal Hoseini <jalal7h@gmail.com>
2017-06-08 02:43:10 +0430

    .

--- components/billing-4/slug.php
+++ components/billing-4/slug.php
@@ -10,7 +10,7 @@ add_slug([
 
 	$client.'/invoice/$' => _URL.'/?page=14&do_slug=payment&invoice_id=$1' ,
 	$client.'/payment/redirect/$' => _URL.'/?page=14&do_slug=payment&do2=redirect&invoice_id=$1',
-	$client.'/payment/verify/$' => _URL.'/?page=14&do_slug=payment&do2=redirect&invoice_id=$1&do3=',
+	$client.'/payment/verify/$' => _URL.'/?page=14&do_slug=payment&do2=verify&invoice_id=$1',
 
 	'admin/billing/user/$/$' => _URL.'/?page=admin&cp=billing_mg&func=billing_management_user&do=invoice_list&id=$1&p=$2' ,
 	'admin/billing/user/$' => _URL.'/?page=admin&cp=billing_mg&func=billing_management_user&do=invoice_list&id=$1' ,
