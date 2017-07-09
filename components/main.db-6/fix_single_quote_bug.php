0f8ff16c871e6dd0b1dfb77819b63f3a69a6ca4f
Jalal Hoseini <jalal7h@gmail.com>
2017-07-09 18:38:16 +0430

    .

--- components/main.db-6/fix_single_quote_bug.php
+++ components/main.db-6/fix_single_quote_bug.php
@@ -1,42 +1,40 @@
 <?php
 
 # jalal7h@gmail.com
-# 2017/07/06
-# 1.1
+# 2017/07/09
+# 1.2
 
+#
+# page
 if( $_REQUEST['page'] ){
-	
-	$_REQUEST['page'] = mb_ereg_replace('[^a-z0-9]+' , '' , $_REQUEST['page'] );
-	
+	$_REQUEST['page'] = ($_REQUEST['page'] == 'admin') ? 'admin' : intval($_REQUEST['page']);
 	if( $_GET['page'] ){
-		$_GET['page'] = mb_ereg_replace('[^a-z0-9]+' , '' , $_GET['page'] );		
-	
+		$_GET['page'] = $_REQUEST['page'];
 	} else if( $_POST['page'] ){
-		$_POST['page'] = mb_ereg_replace('[^a-z0-9]+' , '' , $_POST['page'] );		
+		$_POST['page'] = $_REQUEST['page'];
 	}
-
 }
 
+#
+# etc
 if( $_REQUEST ){
+	
 	foreach ($_REQUEST as $k => $v) {
-		
-		if(! is_string($v) ){
-			continue;
-		} else if(! $v ){
-			continue;
+		if( $v and is_string($v) ){
+			
+			$_REQUEST[$k] = addslashes($v);
+
+			if( $_GET[$k] ){
+				$_GET[$k] = $_REQUEST[$k];
+			
+			} else if( $_POST[$k] ){
+				$_POST[$k] = $_REQUEST[$k];
+			}
+
 		}
-		
-		// $_REQUEST[ $k ] = str_replace( "'", "\'", $v );
-		$_REQUEST[ $k ] = mysqli_escape_string( $GLOBALS['db'], $v );
-
-		if( $_GET[ $k ] ){
-			// $_GET[ $k ] = str_replace( "'", "\'", $v );
-			$_GET[ $k ] = mysqli_escape_string( $GLOBALS['db'], $v );
-
-		} else if( $_POST[ $k ] ){
-			// $_POST[ $k ] = str_replace( "'", "\'", $v );
-			$_POST[ $k ] = mysqli_escape_string( $GLOBALS['db'], $v );
-		}
-
 	}
+
 }
+
+
+
