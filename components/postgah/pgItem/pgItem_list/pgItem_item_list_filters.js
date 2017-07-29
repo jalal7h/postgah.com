a912e0b9450f3fcb92d7a9df30c2a66d43f9b5e9
Jalal Hoseini <jalal7h@gmail.com>
2017-07-30 01:46:41 +0430

    .

--- components/postgah/pgItem/pgItem_list/pgItem_item_list_filters.js
+++ components/postgah/pgItem/pgItem_list/pgItem_item_list_filters.js
@@ -3,23 +3,28 @@ jQuery(document).ready(function($) {
 	
 	$('.pgItem_item_list_filters input[type="button"]').on('click', function(){
 		
-		pf = $('.pgItem_item_list_filters .price_from').val();
-		pt = $('.pgItem_item_list_filters .price_to').val();
+		pf = parseInt( $('.pgItem_item_list_filters .price_from').val() );
+		pt = parseInt( $('.pgItem_item_list_filters .price_to').val() );
 
-		if( pt < pf ){
-			return false;
+		cl( pf + ' -> ' + pt );
+
+		if( pt >= pf ){
+			
+			cl( pt +' is bigger than '+ pf );
+
+			if( !isnumeric(pf) || pf < 0 ){
+				pf = 0;
+			}
+			if( !isnumeric(pt) || pt < 0 ){
+				pt = 'n';
+			}
+			
+			p = pf+'-'+pt;
+			
+			$('.pgItem_item_list_filters input[name="price_range"]').val(p).trigger('change');
+
 		}
 
-		if( !isnumeric(pf) || pf < 0 ){
-			pf = 0;
-		}
-		if( !isnumeric(pt) || pt < 0 ){
-			pt = 'n';
-		}
-		
-		p = pf+'-'+pt;
-		
-		$('.pgItem_item_list_filters input[name="price_range"]').val(p).trigger('change');
 	});
 
 });
